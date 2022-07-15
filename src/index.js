96e84b62f0bcb4beede3c368f030fda93ba891d2
120dd <cjs120dd@gmail.com>
2022-07-15 12:07:39 +0900

    feat:airbnb eslint설정, 버전없는 파일들 추가

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 import {
-    missionUtilGenerator,
-    RandomGenerator,
-    simpleRandomGenerator
-} from './randomGenerator';
+  missionUtilGenerator,
+  RandomGenerator,
+  simpleRandomGenerator,
+} from './randomGenerator.js';
 
-const randomGenerator = new RandomGenerator(simpleRandomGenerator)
+const randomGenerator = new RandomGenerator(simpleRandomGenerator);
 
 const rnd = randomGenerator.getExcludedRandomNum(0, 1, () => false);
 
-
-
-
-///** min~max 범위의 중복되지 않고, 0이 포함되지 않은 숫자의 랜덤값을 만들어 리턴한다
+/// ** min~max 범위의 중복되지 않고, 0이 포함되지 않은 숫자의 랜덤값을 만들어 리턴한다
 // * @param {number} min
 // * @param {number} max
 // * @return {number}
 // */
-//import {convertNumToArr, hasDuplicatedNum, hasZero} from "./utils";
+// import {convertNumToArr, hasDuplicatedNum, hasZero} from "./utils";
 //
-///** excludeFilter 가 true 를 return 하지 않는 min ~ max 사이의 숫자 랜덤값을 만들어 리턴한다
+/// ** excludeFilter 가 true 를 return 하지 않는 min ~ max 사이의 숫자 랜덤값을 만들어 리턴한다
 // * @param {number} min
 // * @param {number} max
 // * @param {Function} excludeFilter (number(generatedNumber) => boolean)
 // * @return {number}
 // */
-//export function getExcludedRandomNum(min, max, excludeFilter) {
+// export function getExcludedRandomNum(min, max, excludeFilter) {
 //    let randomNumber;
 //
 //    do {
@@ -32,9 +29,9 @@ const rnd = randomGenerator.getExcludedRandomNum(0, 1, () => false);
 //    } while (!excludeFilter(randomNumber));
 //
 //    return randomNumber;
-//}
+// }
 //
-//export function getRandomNumber(min, max) {
+// export function getRandomNumber(min, max) {
 //    let randomNumber;
 //    let randomNumberArr;
 //    do {
@@ -43,10 +40,10 @@ const rnd = randomGenerator.getExcludedRandomNum(0, 1, () => false);
 //        randomNumberArr = convertNumToArr(randomNumber);
 //    } while (hasDuplicatedNum(randomNumber) || hasZero(randomNumberArr));
 //    return randomNumber;
-//}
+// }
 //
-//// 테스트용 임시 함수
-//function getRandomArbitrary(min, max) {
+/// / 테스트용 임시 함수
+// function getRandomArbitrary(min, max) {
 //    const result = Math.random() * (max - min) + min;
 //    return Math.floor(result);
-//}
+// }
